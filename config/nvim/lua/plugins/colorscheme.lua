64cfc380376e5b2ce7008d693641bb7ba9416197
charanvims <scharan2718@gmail.com>
2025-09-26 21:22:36 +0530

    Added everforest neovim config.

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,19 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
-  {
-    "rose-pine/neovim",
-    opts = {
-      variant = "moon",
-      dark_variant = "moon",
-      disable_background = true,
-      disable_float_background = true,
-      highlight_groups = {
-        Normal = { bg = "#000000" },
-        NormalNC = { bg = "#000000" },
-        SignColumn = { bg = "#000000" },
-        LineNr = { bg = "#000000" },
-        CursorLineNr = { bg = "#000000" },
-        FoldColumn = { bg = "#000000" },
-        NormalFloat = { bg = "#000000" },
-        FloatBorder = { bg = "#000000" },
-      },
-    },
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Select dark mode and medium contrast
+      vim.o.background = "dark"
+      vim.g.everforest_background = "hard"
+
+      -- Optional tweaks
+      vim.g.everforest_enable_italic = false
+      vim.g.everforest_better_performance = 1
+
+      -- Apply colorscheme
+      vim.cmd.colorscheme("everforest")
+    end,
   },
 }
